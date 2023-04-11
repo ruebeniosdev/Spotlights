5b7bfc2a4020d46a83151214330ee276ac9b9904
Rueben <akankobatengr@gmail.com>
2023-04-11 16:44:30 +0000

    Initial Commit

---
+++ Spotlights/SpotlightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpotlightsApp.swift
+//  Spotlights
+//
+//  Created by Rueben on 11/04/2023.
+//
+
+import SwiftUI
+
+@main
+struct SpotlightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
