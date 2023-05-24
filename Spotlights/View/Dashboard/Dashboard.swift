9f79d004c6ea3140d0961a7ee8f6dc9af96e1fb9
Rueben <akankobatengr@gmail.com>
2023-05-24 04:51:17 +0000

    refactored the code

--- Spotlights/View/Dashboard/Dashboard.swift
+++ Spotlights/View/Dashboard/Dashboard.swift
@@ -17,81 +17,12 @@ struct Dashboard: View {
             VStack(spacing: 0) {
                 Header(title: "Spotlights", action: {showSlideMenu = true})
                 ScrollView(.vertical, showsIndicators: false) {
-                    VStack(alignment:  .leading,spacing: 24) {
-                        VStack(alignment:  .leading,spacing: 24) {
-                            Text("Gifters of the week")
-                                .font(.custom("Poppins-SemiBold", size: 20))
-                                .padding(.top)
-                                .padding(.horizontal)
-                            ScrollView(.horizontal, showsIndicators: false) {
-                                HStack {
-                                    GiftersofWeek(image: "gifters", name: "JoeOkat", checkmarkType: true)
-                                    GiftersofWeek(image: "agatha", name: "Agathat", checkmarkType: false)
-                                    GiftersofWeek(image: "1", name: "Pilotyras", checkmarkType: false)
-                                    
-                                }
-                                .padding(.horizontal)
-                            }
-                            .foregroundColor(.white)
-                            VStack(alignment:  .leading,spacing: 24) {
-                                Text("Creators of the week")
-                                    .font(.custom("Poppins-SemiBold", size: 20))
-                                   
-                                HStack(spacing: 11) {
-                                    CreatorPeopleCard(title: "Pilotyras", check: "ccheck", image: "1")
-                                    CreatorPeopleCard(title: "Onipanua", check: "logo", image: "2")
-                                }
-                                .frame(height: 182)
-                            }
-                            .padding(.horizontal)
-                          
-                        }
-                    
-                        .foregroundColor(.white)
-                       
-                        VStack(alignment:  .leading,spacing: 24) {
-                            Text("Projects to support")
-                                .font(.custom("Poppins-SemiBold", size: 20))
-                                .padding(.horizontal)
-                            ScrollView(.vertical, showsIndicators: false) {
-                                VStack(spacing: 11) {
-                                    HStack(spacing: 11) {
-                                        NavigationLink {
-                                            OrphanageDetailView()
-                                        } label: {
-                                            ProjectCard(title: "Orphanage", image: "magic-star", bgcolor: Color("offwhite"))
-                                        }
-                                        
-                                        NavigationLink {
-                                            BusinessDetailVIew()
-                                        } label: {
-                                            ProjectCard(title: "Business", image: "presention-chart", bgcolor: Color("accent"))
-                                        }
-                                    }
-                                    HStack(spacing: 11) {
-                                        NavigationLink {
-                                            SchoolDetail()
-                                        } label: {
-                                            ProjectCard(title: "School", image: "teacher", bgcolor: Color("primary"))
-                                        }
-                                        
-                                        
-                                        NavigationLink {
-                                            Hospital()
-                                        } label: {
-                                            ProjectCard(title: "Hospitality", image: "hospital", bgcolor: Color("accent3"))
-                                        }
-                                    }
-                                  
-                                }
-                                .padding(.horizontal)
-                            }
-                        }
-                        .foregroundColor(.white)
-                       
+                    VStack(spacing: 24) {
+                        GiftersOfTheWeekSection()
+                        CreatorsOfTheWeekSection()
+                        ProjectsToSupportSection()
                     }
-                   
-                  
+                    
                 }
             }
             .frame(maxWidth: .infinity,maxHeight: .infinity,alignment: .top)
